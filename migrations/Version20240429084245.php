05ab6c2fb27acc1edc34c0d61c3e99edfe96508e
Mikadjx <109203536+Mikadjx@users.noreply.github.com>
2024-04-29 10:45:48 +0200

    gitignore datafixtures

--- migrations/Version20240429084245.php
+++ migrations/Version20240429084245.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240421025431 extends AbstractMigration
+final class Version20240429084245 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240421025431 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE ressources CHANGE res_path image_file VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE ressources ADD image_name VARCHAR(255) DEFAULT NULL, ADD image_size INT DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE ressources CHANGE image_file res_path VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE ressources DROP image_name, DROP image_size, DROP updated_at');
     }
 }
